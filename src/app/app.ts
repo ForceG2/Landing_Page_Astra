9f702d5199a588d7d3895b1d0348dfca1dc546df
GLavrador <guilherme.lavrador@academico.ufs.br>
2025-09-21 20:46:54 -0300

    feat: add seção atrativos na home (app.ts, app.html)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Background } from './background/background';
 import { About } from './about/about';
+import { Attractions } from './attractions/attractions';
 
 @Component({
   selector: 'app-root',
-  imports: [Background, About],
+  imports: [Background, About, Attractions],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
