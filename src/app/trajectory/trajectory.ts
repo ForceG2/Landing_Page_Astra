42ac41a316ff402485d2c6dd0497ef859a8ba953
jg4lves <hermesjgcode@gmail.com>
2025-09-21 10:52:32 -0300

    wip: adiciona imagens no fundo e suas posiçoes

---
+++ src/app/trajectory/trajectory.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trajectory',
+  imports: [],
+  templateUrl: './trajectory.html',
+  styleUrl: './trajectory.css'
+})
+export class Trajectory {
+
+}
