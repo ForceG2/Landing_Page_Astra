9a1fc0e302453a9d035663719589224f237bd1e0
GLavrador <guilherme.lavrador@academico.ufs.br>
2025-09-23 23:47:17 -0300

    feat: add seção perguntas frequentes na home (app.ts, app.html)

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Background } from './background/background';
 import { About } from './about/about';
 import { Attractions } from './attractions/attractions';
+import { Faq } from './faq/faq';
 
 @Component({
   selector: 'app-root',
-  imports: [Background, About, Attractions],
+  imports: [Background, About, Attractions, Faq],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
