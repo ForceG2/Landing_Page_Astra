b7a3f808bb3431674d93796a58887bc4b5ed4d56
jg4lves <hermesjgcode@gmail.com>
2025-09-27 17:04:43 -0300

    fix: correção do angulo do  foguete

--- src/app/trajectory/trajectory.ts
+++ src/app/trajectory/trajectory.ts
@@ -3,28 +3,44 @@ import { Component, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-trajectory',
   templateUrl: './trajectory.html',
-  styleUrls: ['./trajectory.css']
+  styleUrls: ['./trajectory.css'],
 })
 export class Trajectory implements AfterViewInit {
-
   ngAfterViewInit(): void {
-    const rocket = document.querySelector(".rocket") as HTMLElement;
-    const container = document.querySelector(".trajectory-container") as HTMLElement;
-    const popup = document.getElementById("popup") as HTMLElement;
-    const popupTitle = document.getElementById("popup-title") as HTMLElement;
-    const popupDescription = document.getElementById("popup-description") as HTMLElement;
-    const closePopup = document.getElementById("close-popup") as HTMLElement;
+    const rocket = document.querySelector('.rocket') as HTMLElement;
+    const container = document.querySelector('.trajectory-container') as HTMLElement;
+    const popup = document.getElementById('popup') as HTMLElement;
+    const popupTitle = document.getElementById('popup-title') as HTMLElement;
+    const popupDescription = document.getElementById('popup-description') as HTMLElement;
+    const closePopup = document.getElementById('close-popup') as HTMLElement;
 
-    const planetInfo: Record<string, { title: string, description: string, image: string }> = {
-      ".earth": { title: "Terra", 
-        description: "Começamos nossa jornada pelo planeta azul, apreciando oceanos, florestas e cidades vibrantes – o ponto de partida perfeito para nossa viagem.",
-        image: "/assets/images/card_earth.jpeg" },
-      ".moon": { title: "Lua", description: "O satélite da Terra nos recebe com sua superfície prateada e crateras majestosas – uma vista inesquecível que vai te fazer sentir flutuando entre estrelas.",
-        image: "/assets/images/card_moon.jpeg" },
-      ".space_station": { title: "Estação Espacial", description: "Com uma parada na Estação Espacial, um laboratório futurista orbitando o planeta, onde ciência e aventura se encontram.",
-        image: "/assets/images/card_space_station.jpeg" },
-      ".mars": { title: "Marte", description: "O planeta vermelho surge à frente, com suas paisagens áridas e montanhas imponentes – uma experiência única que promete emocionar todos os exploradores do espaço.",
-        image: "/assets/images/card_mars.jpeg" }
+    let origemAtual: string = 'space_station';
+
+    const planetInfo: Record<string, { title: string; description: string; image: string }> = {
+      '.earth': {
+        title: 'Terra',
+        description:
+          'Começamos nossa jornada pelo planeta azul, apreciando oceanos, florestas e cidades vibrantes – o ponto de partida perfeito para nossa viagem.',
+        image: '/assets/images/card_earth.jpeg',
+      },
+      '.moon': {
+        title: 'Lua',
+        description:
+          'O satélite da Terra nos recebe com sua superfície prateada e crateras majestosas – uma vista inesquecível que vai te fazer sentir flutuando entre estrelas.',
+        image: '/assets/images/card_moon.jpeg',
+      },
+      '.space_station': {
+        title: 'Estação Espacial',
+        description:
+          'Com uma parada na Estação Espacial, um laboratório futurista orbitando o planeta, onde ciência e aventura se encontram.',
+        image: '/assets/images/card_space_station.jpeg',
+      },
+      '.mars': {
+        title: 'Marte',
+        description:
+          'O planeta vermelho surge à frente, com suas paisagens áridas e montanhas imponentes – uma experiência única que promete emocionar todos os exploradores do espaço.',
+        image: '/assets/images/card_mars.jpeg',
+      },
     };
 
     const moverFoguete = (alvo: string) => {
@@ -37,7 +53,8 @@ export class Trajectory implements AfterViewInit {
       const distanciaSegura = 50;
 
       const targetX = destinoRect.left - containerRect.left - distanciaSegura + 20;
-      const targetY = destinoRect.top - containerRect.top + destinoRect.height / 2 - rocket.offsetHeight / 2 - 10;
+      const targetY =
+        destinoRect.top - containerRect.top + destinoRect.height / 2 - rocket.offsetHeight / 2 - 10;
 
       let startX = rocket.offsetLeft;
       let startY = rocket.offsetTop;
@@ -47,9 +64,17 @@ export class Trajectory implements AfterViewInit {
       const dy = targetY - startY;
 
       const angle = Math.atan2(dy, dx) * (20 / Math.PI);
-      const anguloDestino = Math.atan2(dy, dx) * (90 / Math.PI);
-      rocket.style.transform = `rotate(${anguloDestino}deg)`;
+      let anguloDestino = Math.atan2(dy, dx) * (90 / Math.PI);
 
+      if (alvo == '.earth' && origemAtual !== 'space_station') {
+        anguloDestino += -180;
+      }
+
+      if (alvo == '.space_station' && origemAtual === 'mars') {
+        anguloDestino += -180;
+      }
+
+      rocket.style.transform = `rotate(${anguloDestino}deg)`;
 
       function animarReta(callback?: () => void) {
         progress += 0.018;
@@ -58,8 +83,8 @@ export class Trajectory implements AfterViewInit {
         const currentX = startX + dx * progress;
         const currentY = startY + dy * progress;
 
-        rocket.style.left = currentX + "px";
-        rocket.style.top = currentY + "px";
+        rocket.style.left = currentX + 'px';
+        rocket.style.top = currentY + 'px';
 
         if (progress < 1) {
           requestAnimationFrame(() => animarReta(callback));
@@ -69,24 +94,24 @@ export class Trajectory implements AfterViewInit {
       }
 
       function animarEstacionamento() {
-        const recuo = 10;    
-        const abaixar = 20;   
+        const recuo = 10;
+        const abaixar = 20;
         const rotacaoFinal = -47;
         let step = 0;
 
         const startPosX = rocket.offsetLeft;
         const startPosY = rocket.offsetTop;
-        const startAngle = dx < 0 ? -angle : angle; 
+        const startAngle = dx < 0 ? -angle : angle;
 
         function animar() {
           step += 0.04;
           if (step > 1) step = 1;
 
           const currentX = startPosX - recuo * Math.sin(Math.PI * step) * step;
-          const currentY = startPosY + abaixar * Math.sin(Math.PI * step / 2);
+          const currentY = startPosY + abaixar * Math.sin((Math.PI * step) / 2);
 
-          rocket.style.left = currentX + "px";
-          rocket.style.top = currentY + "px";
+          rocket.style.left = currentX + 'px';
+          rocket.style.top = currentY + 'px';
 
           const currentAngle = startAngle + (rotacaoFinal - startAngle) * step;
           rocket.style.transform = `rotate(${currentAngle}deg)`;
@@ -94,22 +119,24 @@ export class Trajectory implements AfterViewInit {
           if (step < 1) {
             requestAnimationFrame(animar);
           } else {
+            origemAtual = alvo.substring(1);
+
             const info = planetInfo[alvo];
             if (info) {
               popupTitle.textContent = info.title;
               popupDescription.textContent = info.description;
-              const popupImage = document.getElementById("popup-image") as HTMLImageElement;
+              const popupImage = document.getElementById('popup-image') as HTMLImageElement;
               popupImage.src = info.image;
-              popup.style.display = "flex";
+              popup.style.display = 'flex';
             }
 
             let floatStep = 0;
             function flutuar() {
               floatStep += 0.04;
-              const offsetY = 2 * Math.sin(floatStep * Math.PI); 
-              const offsetX = 2 * Math.sin(floatStep * Math.PI/2); 
-              rocket.style.left = currentX + offsetX + "px";
-              rocket.style.top = currentY + offsetY + "px";
+              const offsetY = 2 * Math.sin(floatStep * Math.PI);
+              const offsetX = 2 * Math.sin((floatStep * Math.PI) / 2);
+              rocket.style.left = currentX + offsetX + 'px';
+              rocket.style.top = currentY + offsetY + 'px';
               requestAnimationFrame(flutuar);
             }
 
@@ -123,13 +150,21 @@ export class Trajectory implements AfterViewInit {
       animarReta(animarEstacionamento);
     };
 
-    document.querySelector(".moon")?.addEventListener("click", () => moverFoguete(".moon"));
-    document.querySelector(".mars")?.addEventListener("click", () => moverFoguete(".mars"));
-    document.querySelector(".space_station")?.addEventListener("click", () => moverFoguete(".space_station"));
-    document.querySelector(".earth")?.addEventListener("click", () => moverFoguete(".earth"));
+    document.querySelector('.moon')?.addEventListener('click', () => {
+      moverFoguete('.moon');
+    });
+    document.querySelector('.mars')?.addEventListener('click', () => {
+      moverFoguete('.mars');
+    });
+    document.querySelector('.space_station')?.addEventListener('click', () => {
+      moverFoguete('.space_station');
+    });
+    document.querySelector('.earth')?.addEventListener('click', () => {
+      moverFoguete('.earth');
+    });
 
-    closePopup?.addEventListener("click", () => {
-      popup.style.display = "none";
+    closePopup?.addEventListener('click', () => {
+      popup.style.display = 'none';
     });
   }
 }
