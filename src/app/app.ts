aa1c598230fda145bbc5c3602545cfc964a97fed
GLavrador <guilherme.lavrador@academico.ufs.br>
2025-09-27 22:29:14 -0300

    feat: add seção hero na home (app.ts, app.html)

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Background } from './background/background';
 import { About } from './about/about';
 import { Attractions } from './attractions/attractions';
 import { Faq } from './faq/faq';
+import { Hero } from './hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [Background, About, Attractions, Faq],
+  imports: [Background, About, Attractions, Faq, Hero],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
