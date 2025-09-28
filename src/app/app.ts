9fb4926429bf7e9fd2b5f923962fb5e639bc51aa
jg4lves <hermesjgcode@gmail.com>
2025-09-28 12:39:02 -0300

    feat: add formulario com texto a esquerda

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Background } from './background/background';
 import { About } from './about/about';
 import { Attractions } from './attractions/attractions';
 import { Faq } from './faq/faq';
+import { Forms } from "./forms/forms";
 
 @Component({
   selector: 'app-root',
-  imports: [Background, About, Attractions, Faq],
+  imports: [Background, About, Attractions, Faq, Forms],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
