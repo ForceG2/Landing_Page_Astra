16aba83a3c4b2e743064b393a1cdceabf124ab22
jg4lves <hermesjgcode@gmail.com>
2025-10-04 15:11:09 -0300

    feat: add entrega de email

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,11 +3,13 @@ import { Background } from './background/background';
 import { About } from './about/about';
 import { Attractions } from './attractions/attractions';
 import { Faq } from './faq/faq';
-import { Forms } from "./forms/forms";
+import { Forms } from './forms/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Background, About, Attractions, Faq, Forms],
+  standalone: true,
+  imports: [CommonModule, Background, About, Attractions, Faq, Forms],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
--- frontend/src/app/forms/forms.ts
+++ frontend/src/app/forms/forms.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+import { sendEmail } from '../service/emailService';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [ CommonModule, ReactiveFormsModule],
   templateUrl: './forms.html',
-  styleUrl: './forms.css'
+  styleUrls: ['./forms.css']
 })
 export class Forms {
+  formData = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    telefone: new FormControl('', Validators.required),
+    dataNascimento: new FormControl('', Validators.required),
+    termos: new FormControl(false, Validators.requiredTrue)
+  });
+
+toastVisible = false;
+
+  async submitForm() {
+    if (this.formData.invalid) {
+      this.formData.markAllAsTouched();
+      return;
+    }
+    try {
+      const response = await sendEmail(this.formData.value);
+      console.log('Email enviado com sucesso:', response);
+      this.showToast();
+
+    } catch (error) {
+      console.error('Erro ao enviar email:', error);
+    }
+  }
+
+  showToast() {
+    this.toastVisible = true;
+    setTimeout(() => {
+      this.toastVisible = false;
+    }, 3000); // toast desaparece após 3 segundos
+  }
 }
