47b61a8458fe65ea2ce08bf381ee8056133eb412
GLavrador <guilherme.lavrador@academico.ufs.br>
2025-10-14 22:23:49 -0300

    feat: add esqueleto seção depoimentos

---
+++ src/app/testimony/testimony.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimony',
+  imports: [],
+  templateUrl: './testimony.html',
+  styleUrl: './testimony.css'
+})
+export class Testimony {
+
+}
