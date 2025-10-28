2112b9f308c6bd1565cd7485d5bbfa97630a808d
jgalves <hermesjgcode@gmail.com>
2025-10-28 15:21:55 -0300

    fix: aumento da label e redução do espaçamento com a seção de baixo

--- src/app/trajectory/trajectory.ts
+++ src/app/trajectory/trajectory.ts
@@ -184,7 +184,7 @@ export class Trajectory implements AfterViewInit {
           const containerRect = container.getBoundingClientRect();
           const offset = labelOffsets[planeta];
           const centerX = rect.left - containerRect.left + rect.width / 2 + offset.x;
-          const centerY = rect.top - containerRect.top + rect.height + offset.y - 5;
+          const centerY = rect.top - containerRect.top + rect.height + offset.y - 9;
           labelEl.style.left = `${centerX}px`;
           labelEl.style.top = `${centerY}px`;
         }
